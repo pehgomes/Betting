d528b525b9e8c9722bab48de692440a2290cab3c
Pedro H. Gomes <pedrogomesup2@gmail.com>
2017-06-03 22:00:00 -0300

    Update ListaAposta.java

--- app/src/main/java/br/com/matheussouza/betting/ListaAposta.java
+++ app/src/main/java/br/com/matheussouza/betting/ListaAposta.java
@@ -16,12 +16,21 @@ public class ListaAposta extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_aposta);
 
-        apostas = Aposta.listAll(Aposta.class);
+        Intent intent = getIntent();
+
+        if (intent != null) {
+            parametro = String.valueOf(intent.getStringExtra("tela"));
+        }
+
+        if (parametro.equals("1")) {
+            apostas = ApostaEntity.listAll(ApostaEntity.class);
+        } else {
+            numeroSorteado = intent.getIntExtra("numeroSorteado", 100);
+            apostas = ApostaEntity.find(ApostaEntity.class, "numero_aposta = ?", String.valueOf(numeroSorteado));
+            Toast.makeText(getApplicationContext(), "O número sorteado foi " + String.valueOf(numeroSorteado), Toast.LENGTH_SHORT).show();
+        }
         listaAposta = (ListView) findViewById(R.id.lista_aposta);
 
         ApostaAdapter adapter = new ApostaAdapter(getApplicationContext(), apostas);
         listaAposta.setAdapter(adapter);
-
-
-    }
 }
