3a22a471a23420033f08367b96445e308950f804
Pedro H. Gomes <pedrogomesup2@gmail.com>
2017-06-03 22:04:32 -0300

    Update ApostaAdapter.java

--- app/src/main/java/br/com/matheussouza/betting/ApostaAdapter.java
+++ app/src/main/java/br/com/matheussouza/betting/ApostaAdapter.java
@@ -38,7 +38,7 @@ public class ApostaAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View view;
+         View view;
 
         if (convertView == null) {
             view = LayoutInflater.from(context).inflate(R.layout.lista_aposta, null);
@@ -46,16 +46,17 @@ public class ApostaAdapter extends BaseAdapter {
             view = convertView;
         }
 
-        Aposta aposta = apostas.get(position);
-        aposta.setNome(apostas.get(position).getNome());
-        aposta.setNumeroAposta(apostas.get(position).getNumeroAposta());
+        ApostaEntity apostaEntity = apostas.get(position);
+        apostaEntity.setNome(apostas.get(position).getNome());
+        apostaEntity.setNumeroAposta(apostas.get(position).getNumeroAposta());
 
         TextView nome = (TextView) view.findViewById(R.id.nome_apostador);
-        //TextView numero = (TextView) view.findViewById(R.id.numero_aposta);
+        TextView numeroAposta = (TextView) view.findViewById(R.id.numero_aposta);
 
-        nome.setText(aposta.getNome());
-        //numero.setText(apostaEntity.getNumeroAposta());
+        nome.setText(apostaEntity.getNome());
+        numeroAposta.setText(String.valueOf(apostaEntity.getNumeroAposta()));
 
         return view;
-    }
+}
+    
 }
